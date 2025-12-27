8214e1b25a1777697c42add60117d7066622357d
Priya Kumari <pandeypriya05052003@gmail.com>
2025-12-27 15:55:49 +0530

    Update Dialogflow credentials handling

--- Backend/dialogflow_helper.py
+++ Backend/dialogflow_helper.py
@@ -1,11 +1,21 @@
-from google.cloud import dialogflow_v2 as dialogflow
-import uuid
 import os
+import json
+from google.cloud import dialogflow_v2 as dialogflow
+from google.oauth2 import service_account
 
-PROJECT_ID = os.getenv("mira-fastfood-tcoq")
+PROJECT_ID = os.getenv("DIALOGFLOW_PROJECT_ID")
+
+credentials_info = json.loads(
+    os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+)
+
+credentials = service_account.Credentials.from_service_account_info(
+    credentials_info
+)
 
 def detect_intent_text(text, session_id):
-    session_client = dialogflow.SessionsClient()
+    session_client = dialogflow.SessionsClient(credentials=credentials)
+
     session = session_client.session_path(PROJECT_ID, session_id)
 
     text_input = dialogflow.TextInput(text=text, language_code="en")
