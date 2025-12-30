c911fd7388715c0d1a0241ba3ab72fe726bc575b
Priya Kumari <pandeypriya05052003@gmail.com>
2025-12-30 14:14:27 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import db_helper
 import generic_helper
-from dialogflow_helper import detect_intent_and_params
+#from dialogflow_helper import detect_intent_and_params
 
 app = FastAPI()
 
@@ -194,4 +194,5 @@ def track_order(parameters: dict, session_id: str):
     return JSONResponse(content={
         "fulfillmentText": f"Order {order_id} is currently {status}."
     })
+
 
